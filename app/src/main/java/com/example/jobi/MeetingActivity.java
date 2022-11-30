7b40d0b97664041894c14f119d313eacdaaf5dbc
Monvet <monvet801@hotmail.com>
2022-11-30 00:52:17 -0600

    Inicio del paso 3

---
+++ app/src/main/java/com/example/jobi/MeetingActivity.java
@@ -0,0 +1,14 @@
+package com.example.jobi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MeetingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meeting);
+    }
+}
